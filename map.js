805f77a1f8a060463a9043289cce669ccee9cb66
Jason Carrillo <jason.carrillo182@gmail.com>
2020-12-10 16:20:45 -0700

    added external js file

--- map.js
+++ map.js
@@ -1,1 +1,9 @@
 const mapLink = "https://maps.googleapis.com/maps/api/js?key=" + mapCode + "&callback=initMap";
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById(map), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
