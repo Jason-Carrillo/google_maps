5a3ce722be4dc26d5e134e7bd3a0efa60d7c758e
Jason Carrillo <jason.carrillo182@gmail.com>
2020-12-14 15:29:49 -0700

    worked with saving waypoints, and using them to route from a to b to c and so on

--- map.js
+++ map.js
@@ -30,64 +30,118 @@
 
 // TESTING WAYPOINTS WITH THE DIRECTIONS
 
+// function initMap() {
+//     const directionsService = new google.maps.DirectionsService();
+//     const directionsRenderer = new google.maps.DirectionsRenderer();
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 6,
+//         center: { lat: 41.85, lng: -87.65 },
+//     });
+//     directionsRenderer.setMap(map);
+//     document.getElementById("submit").addEventListener("click", () => {
+//         calculateAndDisplayRoute(directionsService, directionsRenderer);
+//     });
+//
+//
+// }
+
 function initMap() {
-    const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer();
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6,
-        center: { lat: 41.85, lng: -87.65 },
+        zoom: 4,
+        center: { lat: 32.8205865, lng: -96.8716371 },
     });
-    directionsRenderer.setMap(map);
-    document.getElementById("submit").addEventListener("click", () => {
-        calculateAndDisplayRoute(directionsService, directionsRenderer);
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer({
+        draggable: true,
+        map,
+        panel: document.getElementById("right-panel"),
     });
-
-
+    directionsRenderer.addListener("directions_changed", () => {
+        computeTotalDistance(directionsRenderer.getDirections());
+    });
+    displayRoute(
+        "Ozona, TX",
+        "Sheffield, TX",
+        directionsService,
+        directionsRenderer
+    );
 }
 
+var testPoints = [ {location:{lat: 30.8756133, lng: -101.571342 }},{location: {lat: 30.8650913, lng: -101.6466300}},{ location: {lat: 30.739580, lng:-101.659839}}]
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-    const waypts = [];
-    const checkboxArray = document.getElementById("waypoints");
-
-    for (let i = 0; i < checkboxArray.length; i++) {
-        if (checkboxArray.options[i].selected) {
-            waypts.push({
-                location: checkboxArray[i].value,
-                stopover: true,
-            });
-        }
-    }
-    directionsService.route(
+function displayRoute(origin, destination, service, display) {
+    service.route(
         {
-            origin: document.getElementById("start").value,
-            destination: document.getElementById("end").value,
-            waypoints: waypts,
-            optimizeWaypoints: true,
+            origin: origin,
+            destination: destination,
+            waypoints: testPoints,
             travelMode: google.maps.TravelMode.DRIVING,
+            avoidTolls: true,
         },
-        (response, status) => {
+        (result, status) => {
             if (status === "OK") {
-                directionsRenderer.setDirections(response);
-                const route = response.routes[0];
-                const summaryPanel = document.getElementById("directions-panel");
-                summaryPanel.innerHTML = "";
-
-                // For each route, display summary information.
-                for (let i = 0; i < route.legs.length; i++) {
-                    const routeSegment = i + 1;
-                    summaryPanel.innerHTML +=
-                        "<b>Route Segment: " + routeSegment + "</b><br>";
-                    summaryPanel.innerHTML += route.legs[i].start_address + " to ";
-                    summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
-                    summaryPanel.innerHTML += route.legs[i].distance.text + "<br><br>";
-                }
+                display.setDirections(result);
             } else {
-                window.alert("Directions request failed due to " + status);
+                alert("Could not display directions due to: " + status);
             }
         }
     );
 }
+
+function computeTotalDistance(result) {
+    let total = 0;
+    const myroute = result.routes[0];
+
+    for (let i = 0; i < myroute.legs.length; i++) {
+        total += myroute.legs[i].distance.value;
+    }
+    total = total / 1000;
+    document.getElementById("total").innerHTML = total + " km";
+}
+
+// SEARCH FOR CITY WITH WAYPOINTS IN BETWEEN
+// function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+//     const waypts = [];
+//     const checkboxArray = document.getElementById("waypoints");
+//
+//     for (let i = 0; i < checkboxArray.length; i++) {
+//         if (checkboxArray.options[i].selected) {
+//             waypts.push({
+//                 location: checkboxArray[i].value,
+//                 stopover: true,
+//             });
+//         }
+//     }
+//     directionsService.route(
+//         {
+//             origin: document.getElementById("start").value,
+//             destination: document.getElementById("end").value,
+//             waypoints: waypts,
+//             optimizeWaypoints: true,
+//             travelMode: google.maps.TravelMode.DRIVING,
+//         },
+//         (response, status) => {
+//             if (status === "OK") {
+//                 directionsRenderer.setDirections(response);
+//                 const route = response.routes[0];
+//                 const summaryPanel = document.getElementById("directions-panel");
+//                 summaryPanel.innerHTML = "";
+//
+//                 // For each route, display summary information.
+//                 for (let i = 0; i < route.legs.length; i++) {
+//                     const routeSegment = i + 1;
+//                     summaryPanel.innerHTML +=
+//                         "<b>Route Segment: " + routeSegment + "</b><br>";
+//                     summaryPanel.innerHTML += route.legs[i].start_address + " to ";
+//                     summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
+//                     summaryPanel.innerHTML += route.legs[i].distance.text + "<br><br>";
+//                 }
+//             } else {
+//                 window.alert("Directions request failed due to " + status);
+//             }
+//         }
+//     );
+// }
 
 // function calculateAndDisplayRouteConverse(directionsService, directionsRenderer) {
 //
