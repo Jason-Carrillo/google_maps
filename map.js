a7fcd1591a38ed396c03530206622626ac730738
Jason Carrillo <jason.carrillo182@gmail.com>
2020-12-15 11:17:47 -0700

    when clicking, and adding a marker it now pushed those coordinates to an array

--- map.js
+++ map.js
@@ -98,26 +98,114 @@
 //     total = total / 1000;
 //     document.getElementById("total").innerHTML = total + " km";
 // }
+//
+// var map;
+
+//
+// function initMap() {
+//     var lat_lng = {lat: 34.7062978, lng:  -116.1274117};
+//
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 7,
+//         center: lat_lng,
+//     });
+//
+//     // This event listener will call addMarker() when the map is clicked.
+//     map.addListener('click', function(event) {
+//         addMarker(event.latLng);
+//     });
+//
+//     // Adds a marker at the center of the map.
+//     addMarker(lat_lng);
+// }
+
+
+
+//
+// // Update lat/long value of div when anywhere in the map is clicked
+// google.maps.event.addEventListener(map,'click',function(event) {
+//     document.getElementById('latclicked').innerHTML = event.latLng.lat();
+//     document.getElementById('longclicked').innerHTML =  event.latLng.lng();
+// });
+//
+// // Update lat/long value of div when you move the mouse over the map
+// google.maps.event.addEventListener(map,'mousemove',function(event) {
+//     document.getElementById('latmoved').innerHTML = event.latLng.lat();
+//     document.getElementById('longmoved').innerHTML = event.latLng.lng();
+// });
+
 
 var map;
 var markers = [];
 
 function initMap() {
-    var lat_lng = {lat: 34.7062978, lng:  -116.1274117};
+
+    var latitude = 27.7172453; // YOUR LATITUDE VALUE
+    var longitude = 85.3239605; // YOUR LONGITUDE VALUE
+
+    var myLatLng = {lat: 34.7062978, lng: -116.1274117};
 
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 7,
-        center: lat_lng,
-        mapTypeId: google.maps.MapTypeId.TERRAIN
+        center: myLatLng,
+        zoom: 14,
+        disableDoubleClickZoom: true, // disable the default map zoom on double click
     });
 
-    // This event listener will call addMarker() when the map is clicked.
-    map.addListener('click', function(event) {
-        addMarker(event.latLng);
+    // Update lat/long value of div when anywhere in the map is clicked
+    google.maps.event.addListener(map,'click',function(event) {
+        document.getElementById('latclicked').innerHTML = event.latLng.lat();
+        document.getElementById('longclicked').innerHTML =  event.latLng.lng();
     });
 
-    // Adds a marker at the center of the map.
-    addMarker(lat_lng);
+    // Update lat/long value of div when you move the mouse over the map
+    google.maps.event.addListener(map,'mousemove',function(event) {
+        document.getElementById('latmoved').innerHTML = event.latLng.lat();
+        document.getElementById('longmoved').innerHTML = event.latLng.lng();
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        //title: 'Hello World'
+
+        // setting latitude & longitude as title of the marker
+        // title is shown when you hover over the marker
+        title: latitude + ', ' + longitude
+    });
+
+    // Update lat/long value of div when the marker is clicked
+    marker.addListener('click', function(event) {
+        document.getElementById('latclicked').innerHTML = event.latLng.lat();
+        document.getElementById('longclicked').innerHTML =  event.latLng.lng();
+    });
+
+    // Create new marker on double click event on the map
+    google.maps.event.addListener(map,'dblclick',function(event) {
+        var marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map,
+            title: event.latLng.lat()+', '+event.latLng.lng()
+        });
+
+        // Update lat/long value of div when the marker is clicked
+        marker.addListener('click', function() {
+            document.getElementById('latclicked').innerHTML = event.latLng.lat();
+            document.getElementById('longclicked').innerHTML =  event.latLng.lng();
+        });
+    });
+
+    // Create new marker on single click event on the map
+    google.maps.event.addListener(map,'click',function(event) {
+        var marker = new google.maps.Marker({
+          position: event.latLng,
+          map: map,
+          title: event.latLng.lat()+', '+event.latLng.lng()
+
+        });
+        markers.push("Latitutde: " + event.latLng.lat() +" Longitude: "+ event.latLng.lng());
+    });
+
+
 }
 
 // Adds a marker to the map and push to the array.
@@ -126,7 +214,7 @@ function addMarker(location) {
         position: location,
         map: map
     });
-    markers.push(marker);
+
 }
 
 
