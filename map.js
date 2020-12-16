94b26928cd2d382c5b1173dbafa9019eac80ba53
Jason Carrillo <jason.carrillo182@gmail.com>
2020-12-16 09:08:35 -0700

    got full direcetions from point a, to point b to work with user input coordinates to display

--- map.js
+++ map.js
@@ -119,108 +119,126 @@
 var map;
 var markers = [];
 
-function initMap() {
+// TEST COORDINATES
+// {lat: 34.7062978, lng: -116.1274117}
 
-    var latitude = 34.7062978; // YOUR LATITUDE VALUE
-    var longitude = -116.1274117; // YOUR LONGITUDE VALUE
 
-    var myLatLng = {lat: 34.7062978, lng: -116.1274117};
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 14,
-        disableDoubleClickZoom: true, // disable the default map zoom on double click
-    });
 
-    // Update lat/long value of div when anywhere in the map is clicked
-    google.maps.event.addListener(map,'click',function(event) {
-        document.getElementById('latclicked').innerHTML = event.latLng.lat();
-        document.getElementById('longclicked').innerHTML =  event.latLng.lng();
-    });
+
 
     // Update lat/long value of div when you move the mouse over the map
-    google.maps.event.addListener(map,'mousemove',function(event) {
-        document.getElementById('latmoved').innerHTML = event.latLng.lat();
-        document.getElementById('longmoved').innerHTML = event.latLng.lng();
-    });
+    // google.maps.event.addListener(map, 'mousemove', function (event) {
+    //     document.getElementById('latmoved').innerHTML = event.latLng.lat();
+    //     document.getElementById('longmoved').innerHTML = event.latLng.lng();
+    // });
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        //title: 'Hello World'
 
-        // setting latitude & longitude as title of the marker
-        // title is shown when you hover over the marker
-        title: latitude + ', ' + longitude
-    });
+    // ADDS A MARKER WHEN INITIALLY GOING TO THE PAGE
+    // var marker = new google.maps.Marker({
+    //     position: {lat: 34.7062978, lng: -116.1274117},
+    //     map: map,
+    //     //title: 'Hello World'
+    //
+    //     // setting latitude & longitude as title of the marker
+    //     // title is shown when you hover over the marker
+    //     title: latitude + ', ' + longitude
+    // });
 
     // Update lat/long value of div when the marker is clicked
-    marker.addListener('click', function(event) {
-        document.getElementById('latclicked').innerHTML = event.latLng.lat();
-        document.getElementById('longclicked').innerHTML =  event.latLng.lng();
-    });
+    // marker.addListener('click', function (event) {
+    //     document.getElementById('latclicked').innerHTML = event.latLng.lat();
+    //     document.getElementById('longclicked').innerHTML = event.latLng.lng();
+    // });
 
-    var currentId = 0;
-    var uniqueId = function() {
-        return ++currentId;
-    }
-    var objLoc = {};
-    // Create new marker on single click event on the map
-    google.maps.event.addListener(map,'click',function(event) {
-        var id = uniqueId(); // get new id
-        var marker = new google.maps.Marker({
-            id: id,
-            position: event.latLng,
-            map: map,
-            title: event.latLng.lat()+', '+event.latLng.lng()
+    // var currentId = 0;
+    // var uniqueId = function () {
+    //     return ++currentId;
+    // }
+    // var objLoc = {};
+    // // Create new marker on single click event on the map
+    // google.maps.event.addListener(map, 'click', function (event) {
+    //     var id = uniqueId(); // get new id
+    //     var marker = new google.maps.Marker({
+    //         id: id,
+    //         position: event.latLng,
+    //         map: map,
+    //         title: event.latLng.lat() + ', ' + event.latLng.lng()
+    //
+    //     });
+    //     var obj = {};
+    //
+    //     obj["lat"] = event.latLng.lat();
+    //     obj["lng"] = event.latLng.lng();
+    //     markers.push(obj);
+    //     Object.assign(objLoc, obj)
+    //
+    //
+    //     console.log(objLoc)
+    //     console.log(markers)
+    // });
 
-        });
-        var obj = {};
 
-        obj["lat"] = event.latLng.lat();
-        obj["lng"] = event.latLng.lng();
-        markers.push(obj);
-        Object.assign(objLoc, obj)
-
-
-        console.log(objLoc)
-        console.log(markers)
-    });
-
-
-}
-
-var realPoints = [{lat: 34.70171128524146, lng: -116.14878354404999}, {lat: 34.70324799225423, lng: -116.141402104841}, {lat: 34.70120165679538, lng: -116.12518010471894}, {lat: 34.705929320728494, lng: -116.1165112051828}]
 
 
 // THIS WILL TURN COORDINATES THAT WHERE ENTERED INTO LOCATION FORMAT TO BE USED IN THE "displayRoute()" function
-var mapRealPoints = realPoints.map(n => {
-    const objTest = { location: n};
-    return objTest
-});
+//     var mapRealPoints = realPoints.map(n => {
+//         const objTest = {location: n};
+//         return objTest
+//     });
+//
+//
+//     var testPoints = [{location: {lat: 30.8756133, lng: -101.571342}},
+//         {location: {lat: 30.8650913, lng: -101.6466300}},
+//         {location: {lat: 30.739580, lng: -101.659839}}]
 
-console.log(mapRealPoints)
 
-var testPoints = [ {location:{lat: 30.8756133, lng: -101.571342 }},{location: {lat: 30.8650913, lng: -101.6466300}},{ location: {lat: 30.739580, lng:-101.659839}}]
 
-function displayRoute(origin, destination, service, display) {
-    service.route(
-        {
-            origin: origin,
-            destination: destination,
-            waypoints: mapRealPoints,
-            travelMode: google.maps.TravelMode.DRIVING,
-            avoidTolls: true,
-        },
-        (result, status) => {
-            if (status === "OK") {
-                display.setDirections(result);
-            } else {
-                alert("Could not display directions due to: " + status);
-            }
-        }
-    );
-}
+
+
+// }
+
+
+    // ADD WAY POINTS IN THE MIDDLE OF A ROUTE
+    // function displayRoute(origin, destination, service, display) {
+    //     service.route(
+    //         {
+    //             origin: origin,
+    //             destination: destination,
+    //             waypoints: mapRealPoints,
+    //             travelMode: google.maps.TravelMode.DRIVING,
+    //             avoidTolls: true,
+    //         },
+    //         (result, status) => {
+    //             if (status === "OK") {
+    //                 display.setDirections(result);
+    //             } else {
+    //                 alert("Could not display directions due to: " + status);
+    //             }
+    //         }
+    //     );
+    // }
+
+// }
+
+// function displayRouteTest(origin, destination, service, display) {
+//     service.route(
+//         {
+//             origin: origin,
+//             destination: destination,
+//             waypoints: mapRealPoints,
+//             travelMode: google.maps.TravelMode.DRIVING,
+//             avoidTolls: true,
+//         },
+//         (result, status) => {
+//             if (status === "OK") {
+//                 display.setDirections(result);
+//             } else {
+//                 alert("Could not display directions due to: " + status);
+//             }
+//         }
+//     );
+// }
 
 
 // // Adds a marker to the map and push to the array.
@@ -357,7 +375,53 @@ function displayRoute(origin, destination, service, display) {
 // }
 
 
-// document.getElementById("converse").addEventListener("click", calculateAndDisplayRouteConverse)
-//
-// document.getElementById("El_Paso").addEventListener("click", calculateAndDisplayRouteElPaso)
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: {lat: 34.7062978, lng: -116.1274117},
+    });
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer({
+        draggable: true,
+        map,
+        panel: document.getElementById("right-panel"),
+    });
+
+    displayRoute(
+        "El Paso, TX",
+        "Dallas, TX",
+        directionsService,
+        directionsRenderer
+    );
+}
+
+var realPoints = [{lat: 34.70171128524146, lng: -116.14878354404999}, {
+    lat: 34.70324799225423,
+    lng: -116.141402104841
+}, {lat: 34.70120165679538, lng: -116.12518010471894}, {lat: 34.705929320728494, lng: -116.1165112051828}]
+
+function displayRoute(origin, destination, service, display) {
+    realPoints = realPoints.map(n => {
+        const markerMapped = {location: n};
+        return markerMapped
+    });
+    console.log(realPoints)
+
+    service.route(
+        {
+            origin: origin,
+            destination: destination,
+            waypoints: realPoints,
+            travelMode: google.maps.TravelMode.DRIVING,
+            avoidTolls: true,
+        },
+        (result, status) => {
+            if (status === "OK") {
+                display.setDirections(result);
+            } else {
+                alert("Could not display directions due to: " + status);
+            }
+        }
+    );
+}
 
